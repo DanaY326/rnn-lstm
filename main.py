7c0a3d79350f6cd72c8e8aca1a44aca1642a0a54
Dana Yuan <danayuan2006@gmail.com>
2025-08-18 11:10:13 -0400

    Finished training loop like tutorial (not working)

--- main.py
+++ main.py
@@ -26,4 +26,65 @@ def encode_and_pad(text):
     encoded = [word_to_idx[word] for word in text]
     return encoded + [0] * (max_length - len(encoded))
 
+train_data['text'] = train_data['text'].apply(encode_and_pad)
+test_data['text'] = test_data['text'].apply(encode_and_pad)
 
+class SentimentDataset(Dataset):
+    def __init__(self, data):
+        self.texts = data['text'].values
+        self.labels = data['label'].values
+
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.texts[idx]
+        return torch.tensor(text, dtype=torch.long), torch.tensor(label, dtype=torch.long)
+    
+train_dataset = SentimentDataset(train_data)
+test_dataset = SentimentDataset(test_data)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+class SentimentRNN(nn.Module):
+    def __init__(self, vocab_size, embed_size, hidden_size, output_size):
+        super(SentimentRNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.RNN(embed_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        h0 = torch.zeros(1, x.size(0), hidden_size).to(x.device)
+        out, _ = self.rnn(x, h0)
+        out = self.fc(out[:, -1, :])
+        return out
+    
+vocab_size = len(vocab) + 1
+embed_size = 128
+hidden_size = 128
+output_size = 2
+model = SentimentRNN(vocab_size, embed_size, hidden_size, output_size)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 10
+model.train()
+for epoch in range(num_epochs):
+    epoch_loss = 0
+    for texts, labels in train_loader:
+        
+        print(labels)
+        outputs = model(texts)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss / len(train_loader):.4f}')
