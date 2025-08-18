3f124a14a4ca4106b7da8b8fffc17d6263065fa0
Dana Yuan <danayuan2006@gmail.com>
2025-08-17 20:45:02 -0400

    Continued with tutorial up to encode-and-pad

--- main.py
+++ main.py
@@ -8,5 +8,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset, DataLoader
 
-df = pd.read_csv("/content/IMDB Dataset.csv", names=["text", "label"])
+df = pd.read_csv("content/IMDB Dataset.csv", names=["text", "label"])
+
+df['text'] = df['text'].str.lower().str.split()
+
+le = LabelEncoder()
+df['label'] = le.fit_transform(df['label'])
+
+train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+
+vocab = {word for phrase in df['text'] for word in phrase}
+word_to_idx = {word: idx for idx, word in enumerate(vocab, start=1)}
+
+max_length = df['text'].str.len().max()
+
+def encode_and_pad(text):
+    encoded = [word_to_idx[word] for word in text]
+    return encoded + [0] * (max_length - len(encoded))
+
 
